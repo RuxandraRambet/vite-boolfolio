6fd106d8eb75b5b21b1c648fae44de903e4dff35
RuxandraRambet <ruxandraramb@gmail.com>
2024-02-17 21:23:13 +0100

    feat: spinner

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -8,6 +8,7 @@ export default {
     },
     data() {
         return {
+            loading: false,
             currentPage: 1,
             projects: [],
             'baseUrl': 'http://127.0.0.1:8000',
@@ -18,6 +19,8 @@ export default {
     },
     methods: {
         getProjects() {
+            this.loading = true;
+
             axios.get(this.baseUrl + this.apiUrl.projects, {
                 params: {
                     page: this.currentPage,
@@ -26,6 +29,8 @@ export default {
                 this.projects = response.data.results.data;
             }).catch(error => {
                 console.log(error);
+            }).finally(() => {
+                this.loading = false;
             });
         },
         nextPage() {
@@ -46,21 +51,30 @@ export default {
     <main>
         <div class="container py-5">
             <h1>My Projects</h1>
-            <div class="row mt-5">
-                <div class="col col-md-4 g-3" v-for="project in projects">
-                   <ProjectCard :project="project" />
+            <div v-if="loading">
+                <div class="spinner-border mx-2" style="width: 3rem; height: 3rem; color:rgb(243, 120, 6)" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+                <div class="spinner-grow" style="width: 3rem; height: 3rem; color:rgb(3, 3, 58)" role="status">
+                    <span class="visually-hidden">Loading...</span>
                 </div>
             </div>
-            <nav class="my-5">
-                <ul class="list-unstyled d-flex justify-content-between">
-                    <li>
-                        <button class="btn btn-primary" @click="prevPage">Previous</button>
-                    </li>
-                    <li>
-                        <button  class="btn btn-primary" @click="nextPage">Next</button>
-                    </li>
-                </ul>
-            </nav>
+            <div class="row mt-5" v-else>
+                <div class="col col-md-4 g-3" v-for="project in projects">
+                    <ProjectCard :project="project" />
+                </div>
+                <nav class="my-5">
+                    <ul class="list-unstyled d-flex justify-content-between">
+                        <li>
+                            <button class="btn btn-primary" @click="prevPage">Previous</button>
+                        </li>
+                        <li>
+                            <button class="btn btn-primary" @click="nextPage">Next</button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
         </div>
     </main>
 </template>
